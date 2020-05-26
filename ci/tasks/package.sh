0da0718852a11c413aad0364186cea00c7a4488d
Steve Greenberg <spgreenberg@gmail.com>
2020-05-26 09:21:33 -0600

    ignore ci and release-notes when creating zip

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,7 +2,7 @@
 
 version=$(cat version/version)
 pushd source 
-	zip -r sample-app.zip ./* -x manifest.yml
+	zip -r sample-app.zip ./* -x manifest.yml -x ci -x release-notes
 popd
 cp source/sample-app.zip rc/sample-app-${version}.zip
 cp source/manifest.yml rc/manifest-${version}.yml
